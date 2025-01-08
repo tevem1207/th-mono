58a05713bec26b3bf3a57faceeaed074ea48a00e
Taehee_Hwang <tevem1207@gmail.com>
2025-01-08 16:37:24 +0900

    [feat] typeboy 앱 추가

---
+++ apps/typeboy/components/Editor.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import {
+  ComponentPropsWithoutRef,
+  FocusEventHandler,
+  FormEventHandler,
+  KeyboardEventHandler,
+  useRef,
+} from "react";
+import { useTypingHook } from "./useTypingHook";
+import { formatNumber } from "@repo/util";
+import { cn } from "@repo/ui/lib/utils";
+
+const text =
+  "대충 흑백 사진에 글 쓰면 명언 같다. 대충 흑백 사진에 글 쓰면 명언 같다.";
+
+export const Editor = () => {
+  const { inputText, cpm, accuracy, handleInputChange } = useTypingHook(text);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFormFocus = () => {
+    inputRef.current?.focus();
+  };
+
+  const handleInputFocus: FocusEventHandler<HTMLInputElement> = (event) => {
+    const input = event.target;
+    const length = input.value.length;
+
+    input.setSelectionRange(length, length);
+  };
+
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if ((event.ctrlKey || event.metaKey) && event.key === "a") {
+      event.preventDefault();
+    }
+  };
+
+  const handleFormSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+    console.log("제출");
+  };
+
+  return (
+    <form
+      onFocus={handleFormFocus}
+      onClick={handleFormFocus}
+      className="max-w-[1280px] w-full h-96 border-y-slate-900 border-y-2 mb-20 flex items-center relative"
+      onSubmit={handleFormSubmit}
+    >
+      <div className="px-10 flex flex-wrap">
+        {text.split("").map((char, index) => {
+          const isLast = index === inputText.length - 1;
+          const inputChar = inputText[index];
+
+          return (
+            <div className="flex flex-col mb-4" key={`type-letter-${index}`}>
+              <EditorText
+                className={cn(
+                  "cursor-default",
+                  !isLast &&
+                    inputChar &&
+                    inputChar !== char &&
+                    "text-red-600 font-semibold"
+                )}
+              >
+                {char}
+              </EditorText>
+              {inputChar ? (
+                <EditorText>{inputChar}</EditorText>
+              ) : (
+                <EditorText
+                  isCursor={index === inputText.length}
+                  className="text-gray-300"
+                >
+                  {char}
+                </EditorText>
+              )}
+            </div>
+          );
+        })}
+        <input
+          name="type"
+          className="opacity-0 absolute"
+          autoFocus
+          autoComplete="off"
+          onFocus={handleInputFocus}
+          onKeyDown={handleKeyDown}
+          onChange={handleInputChange}
+          ref={inputRef}
+          value={inputText}
+        />
+      </div>
+
+      <div className="absolute bottom-2 right-4 flex gap-3 items-center">
+        <div className="flex gap-4">
+          <div>SPEED</div>
+          <div>{`${formatNumber(cpm)} CPM`}</div>
+        </div>
+        <div className="h-5 border-r border-gray-900"></div>
+        <div className="flex gap-4">
+          <div>ACC</div>
+          <div>{`${formatNumber(accuracy)} %`}</div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+const EditorText = ({
+  children,
+  className,
+  isCursor,
+  ...props
+}: ComponentPropsWithoutRef<"p"> & { isCursor?: boolean }) => {
+  return (
+    <div className="font-mono flex w-5 text-2xl justify-center">
+      {isCursor && (
+        <span className="border animate-cursor border-gray-900 box-border" />
+      )}
+      {children === " " ? (
+        <p className={cn(!isCursor && "ml-[2px]", className)} {...props}>
+          &nbsp;
+        </p>
+      ) : (
+        <p className={cn(!isCursor && "ml-[2px]", className)} {...props}>
+          {children}
+        </p>
+      )}
+    </div>
+  );
+};
---
+++ apps/typeboy/components/useTypingHook.ts
@@ -0,0 +1,53 @@
+import { ChangeEventHandler, useEffect, useMemo, useState } from "react";
+import { disassemble } from "es-hangul";
+
+export const useTypingHook = (text: string) => {
+  const [inputText, setInputText] = useState("");
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  const cpm =
+    elapsedTime > 1000
+      ? disassemble(inputText).length / (elapsedTime / 60000)
+      : 0;
+
+  const accuracy = useMemo(
+    () =>
+      (inputText
+        .slice(0, -1)
+        .split("")
+        .filter((char, index) => {
+          return char === text[index];
+        }).length /
+        inputText.slice(0, -1).length) *
+      100,
+    [inputText, text]
+  );
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const text = e.target.value;
+    setInputText(text);
+    if (!elapsedTime) {
+      setElapsedTime(1);
+    }
+  };
+
+  const handleReset = () => {
+    setInputText("");
+    setElapsedTime(0);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setElapsedTime((prev) => (prev ? prev + 1000 : prev));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return {
+    inputText,
+    elapsedTime,
+    handleInputChange,
+    handleReset,
+    cpm,
+    accuracy,
+  };
+};
