e5fed5490ed2f8161ba41e740e4a5f03bfdb6584
tevem1207 <tevem1207@gmail.com>
2025-02-18 18:21:09 +0900

    feat: add SignIn component and extend NextAuth handlers

--- apps/typeboy/auth.ts
+++ apps/typeboy/auth.ts
@@ -2,4 +2,6 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-export const { handlers } = NextAuth({ providers: [Google, GitHub] });
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google, GitHub],
+});
