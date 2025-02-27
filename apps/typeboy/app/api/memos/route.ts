32640238741ee2ca22b59644eb7106be88186f69
tevem1207 <tevem1207@gmail.com>
2025-02-27 16:25:00 +0900

    feat: implement pagination for memo retrieval and add POST endpoint for creating memos

--- apps/typeboy/app/api/memos/route.ts
+++ apps/typeboy/app/api/memos/route.ts
@@ -1,20 +1,39 @@
 import { prisma } from "@/lib";
+import { Prisma } from "@prisma/client";
 
 export async function GET(request: Request) {
-  const userId = request.headers.get("Authorization") || "";
-  console.log(userId);
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") || "0");
+  const pageSize = parseInt(url.searchParams.get("pageSize") || "10");
+  const skip = page * pageSize;
+
   const sentences = await prisma.memo.findMany({
+    skip: skip,
+    take: pageSize,
     include: {
       user: true,
       likes: true,
     },
   });
 
-  const result = sentences.map((sentence) => ({
-    ...sentence,
-    isLiked: sentence.likes.some((like) => like.userId === userId),
-    likeCount: sentence.likes.length,
-  }));
+  const totalMemos = await prisma.memo.count();
+  const totalPages = Math.ceil(totalMemos / pageSize);
 
-  return Response.json(result);
+  return Response.json({
+    data: sentences,
+    pagination: {
+      page,
+      pageSize,
+      totalPages,
+      totalMemos,
+    },
+  });
+}
+
+export async function POST(request: Request) {
+  const data = (await request.json()) as Prisma.MemoCreateInput;
+
+  const newMemo = await prisma.memo.create({ data });
+
+  return new Response(JSON.stringify(newMemo), { status: 201 });
 }
