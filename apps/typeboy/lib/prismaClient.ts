d9b1ca1caac5af6de4e23898139597221ab8255d
tevem1207 <tevem1207@gmail.com>
2025-03-02 20:46:39 +0900

    feat: extend PrismaClient with random extension

--- apps/typeboy/lib/prismaClient.ts
+++ apps/typeboy/lib/prismaClient.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import prismaRandom from "prisma-extension-random";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(prismaRandom());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
