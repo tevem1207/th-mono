cf8bec9505c60eacbf6d18681bafcee2c2342193
tevem1207 <tevem1207@gmail.com>
2025-03-02 20:51:06 +0900

    feat: remove prisma-extension-random

--- apps/typeboy/lib/prismaClient.ts
+++ apps/typeboy/lib/prismaClient.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import prismaRandom from "prisma-extension-random";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(prismaRandom());
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
