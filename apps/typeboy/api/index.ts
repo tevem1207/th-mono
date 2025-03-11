c5ff9d6fe76b831d93b67cc416b9da4d3ede2351
tevem1207 <tevem1207@gmail.com>
2025-03-12 01:27:20 +0900

    feat: update fetcher to use dynamic BASE_URL based on NODE_ENV

--- apps/typeboy/api/index.ts
+++ apps/typeboy/api/index.ts
@@ -1,6 +1,9 @@
 export const fetcher = async (url: string) => {
   if (typeof window === "undefined") {
-    const BASE_URL = process.env.VERCEL_URL || "http://localhost:3000";
+    const BASE_URL =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : `https://${process.env.VERCEL_URL}`;
     url = `${BASE_URL}${url}`;
   }
 
