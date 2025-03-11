3f6c48811d46f8066b39c24487d0651a85e18675
tevem1207 <tevem1207@gmail.com>
2025-03-12 00:43:07 +0900

    feat: update API URL handling to use Vercel environment variable

--- apps/typeboy/api/index.ts
+++ apps/typeboy/api/index.ts
@@ -1,9 +1,9 @@
 export * from "./memo";
 
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+export const BASE_URL = process.env.VERCEL_URL || "http://localhost:3000";
+
 export const fetcher = async (url: string) => {
-  const res = await fetch(url);
+  const res = await fetch(BASE_URL + url);
   if (!res.ok) {
     throw new Error("An error occurred while fetching the data.");
   }
--- apps/typeboy/api/memo.ts
+++ apps/typeboy/api/memo.ts
@@ -1,24 +1,14 @@
 import { MemoExtended, PaginationResult } from "@/types";
-import { BASE_URL, fetcher } from ".";
+import { fetcher } from ".";
 
 export const fetchMemos = async (): Promise<PaginationResult<MemoExtended>> => {
-  return fetcher(`${BASE_URL}/api/memos`);
+  return fetcher(`/api/memos`);
 };
 
 export const fetchRandomMemos = async (): Promise<MemoExtended[]> => {
-  return fetcher(`${BASE_URL}/api/memos?random=true&pageSize=${5}`);
+  return fetcher(`/api/memos?random=true&pageSize=${5}`);
 };
 
 export const fetchMemo = async (id: string): Promise<MemoExtended> => {
-  return fetcher(`${BASE_URL}/api/memos/${id}`);
-};
-
-export const submitMemo = async (inputText: string) => {
-  const response = await fetch(`${BASE_URL}/api/submit`, {
-    method: "POST",
-    body: JSON.stringify({ inputText }),
-  });
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
+  return fetcher(`/api/memos/${id}`);
 };
