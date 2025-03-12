9a10ced8f80f9b972b7e07a007ae2c660afef5e5
tevem1207 <tevem1207@gmail.com>
2025-03-12 13:09:04 +0900

    feat: change memos api to sentences

--- apps/typeboy/api/memo.ts
+++ apps/typeboy/api/memo.ts
@@ -2,19 +2,19 @@ import { MemoExtended, PaginationResult } from "@/types";
 import { fetcher } from ".";
 
 export const fetchMemos = async (): Promise<PaginationResult<MemoExtended>> => {
-  return fetcher(`/api/memos`);
+  return fetcher(`/api/sentences`);
 };
 
 export const fetchRandomMemos = async (): Promise<MemoExtended[]> => {
-  return fetcher(`/api/memos?random=true&pageSize=${5}`);
+  return fetcher(`/api/sentences?random=true&pageSize=${5}`);
 };
 
 export const fetchMemo = async (id: string): Promise<MemoExtended> => {
-  return fetcher(`/api/memos/${id}`);
+  return fetcher(`/api/sentences/${id}`);
 };
 
 export const submitMemo = async (text: string) => {
-  return fetch(`/api/memos`, {
+  return fetch(`/api/sentences`, {
     method: "POST",
     body: JSON.stringify({ text }),
   });
