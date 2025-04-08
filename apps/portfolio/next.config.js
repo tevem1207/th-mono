6f61af73e1023c9efeb6f6d9f5decb4ec71f4fab
tevem1207 <tevem1207@gmail.com>
2025-04-09 01:44:53 +0900

    feat: remove production redirect configuration from next.config.js

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  redirects: async () => {
-    if (process.env.NODE_ENV === "production") {
-      return [
-        {
-          source: "/",
-          destination: "https://th-front-portfolio.web.app/",
-          permanent: true,
-        },
-      ];
-    }
-    return [];
-  },
   images: {
     remotePatterns: [
       {
